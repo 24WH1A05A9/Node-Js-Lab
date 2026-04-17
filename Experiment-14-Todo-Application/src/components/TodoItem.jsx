6201b9e3cf5e3805ce8fbdfe19de43b6be9ce98f
24WH1A05A9 <24wh1a05a9@bvrithyderabad.edu.in>
2026-04-17 14:54:13 +0530

    Create TodoItem.jsx

---
+++ Experiment-14-Todo-Application/src/components/TodoItem.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+function TodoItem({
+  todo,
+  toggleTodo,
+  deleteTodo,
+  editTodo,
+  toggleEdit,
+}) {
+  const [newText, setNewText] = useState(todo.text);
+
+  return (
+    <div className="todo-item">
+      {/* CHECKBOX / TEXT */}
+      <div className="left">
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => toggleTodo(todo.id)}
+        />
+
+        {!todo.isEditing ? (
+          <span
+            className={todo.completed ? "done" : ""}
+            onDoubleClick={() => toggleEdit(todo.id)}
+          >
+            {todo.text}
+          </span>
+        ) : (
+          <input
+            type="text"
+            value={newText}
+            onChange={(e) => setNewText(e.target.value)}
+          />
+        )}
+      </div>
+
+      {/* ACTIONS */}
+      <div className="actions">
+        {!todo.isEditing ? (
+          <button onClick={() => toggleEdit(todo.id)}>Edit</button>
+        ) : (
+          <button
+            onClick={() => {
+              if (!newText.trim()) return;
+              editTodo(todo.id, newText);
+            }}
+          >
+            Save
+          </button>
+        )}
+
+        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem;
